9bb406ccd74e4f5167754ee01d826df6f875188c
tike <puodsea@gmail.com>
2017-08-11 16:56:16 +0900

    Add bare PhrasesComponent

---
+++ src/app/phrases/phrases.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gp-phrases',
+  templateUrl: './phrases.component.html',
+  styleUrls: ['./phrases.component.scss']
+})
+export class PhrasesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
