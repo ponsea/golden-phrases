2c33957ca2b6a48bdd7a843c20a229c478c10ae3
tike <puodsea@gmail.com>
2017-08-24 14:08:52 +0900

    Add PhrasesComponent

--- src/app/phrases/phrases.component.ts
+++ src/app/phrases/phrases.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./phrases.component.scss']
 })
 export class PhrasesComponent implements OnInit {
+  titles = [
+    "600点レベル 助走の400語",
+    "730点レベル 加速の300語",
+    "860点レベル 飛躍の200語",
+    "990点レベル 頂点の100語"
+  ]
 
   constructor() { }
 
--- src/app/phrases/phrases.module.ts
+++ src/app/phrases/phrases.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { PhrasesRoutingModule } from './phrases-routing.module';
 import { PhrasesComponent } from './phrases.component';
+import { SectionListComponent } from './section-list/section-list.component';
+import { PhrasesService } from './phrases.service';
 
 @NgModule({
   imports: [
     SharedModule,
     PhrasesRoutingModule,
   ],
-  declarations: [PhrasesComponent]
+  declarations: [
+    PhrasesComponent,
+    SectionListComponent
+  ],
+  providers: [
+    PhrasesService
+  ]
 })
 export class PhrasesModule { }
