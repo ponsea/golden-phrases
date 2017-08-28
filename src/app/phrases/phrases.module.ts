8b2a129dc5f440cda313651f47122061dd04d655
tike <puodsea@gmail.com>
2017-08-28 17:04:11 +0900

    Add some components and services to PhrasesModule

--- src/app/phrases/phrases.module.ts
+++ src/app/phrases/phrases.module.ts
@@ -5,6 +5,9 @@ import { PhrasesRoutingModule } from './phrases-routing.module';
 import { PhrasesComponent } from './phrases.component';
 import { SectionListComponent } from './section-list/section-list.component';
 import { PhrasesService } from './phrases.service';
+import { PhraseConversionService } from './phrase-conversion.service';
+import { SectionComponent } from './section/section.component';
+import { PhraseComponent } from './phrase/phrase.component';
 
 @NgModule({
   imports: [
@@ -13,10 +16,13 @@ import { PhrasesService } from './phrases.service';
   ],
   declarations: [
     PhrasesComponent,
-    SectionListComponent
+    SectionListComponent,
+    SectionComponent,
+    PhraseComponent
   ],
   providers: [
-    PhrasesService
+    PhrasesService,
+    PhraseConversionService
   ]
 })
 export class PhrasesModule { }
