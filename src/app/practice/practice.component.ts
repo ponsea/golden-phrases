db07c2509ee250504a80448756ff36f20cec0fca
tike <puodsea@gmail.com>
2017-08-29 13:37:13 +0900

    Add bare PracticeComponent

---
+++ src/app/practice/practice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gp-practice',
+  templateUrl: './practice.component.html',
+  styleUrls: ['./practice.component.scss']
+})
+export class PracticeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
