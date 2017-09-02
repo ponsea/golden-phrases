b2441378c005d7d7a4d2abb78958162b9c61de54
tike <puodsea@gmail.com>
2017-09-02 14:01:42 +0900

    Use PhrasesViewerComponent in SectionComponent

--- src/app/phrases/section/section.component.ts
+++ src/app/phrases/section/section.component.ts
@@ -11,7 +11,6 @@ import { PhrasesService } from '../../core/phrases.service';
 export class SectionComponent implements OnInit {
   @Input() section: number;
   phrases: Phrase[] = [];
-  phraseIdx = 0;
 
   constructor(private phrasesService: PhrasesService) { }
 
@@ -19,22 +18,5 @@ export class SectionComponent implements OnInit {
     this.phrasesService.getSection(this.section)
       .subscribe(phrases => this.phrases = phrases,
                  err => alert(err.message));
-  }
-
-  get currentPhrase() { return this.phrases[this.phraseIdx]; }
-
-  next() {
-    if (this.phraseIdx < this.phrases.length - 1) this.phraseIdx++;
-  }
-
-  prev() {
-    if (this.phraseIdx > 0) this.phraseIdx--;
-  }
-
-  @HostListener('document:keydown', ['$event'])
-  public onKeyDown(event: KeyboardEvent): void {
-    const KEY_LEFT = 37, KEY_RIGHT = 39;
-    if (event.keyCode === KEY_LEFT) this.prev();
-    if (event.keyCode === KEY_RIGHT) this.next();
   }
 }
