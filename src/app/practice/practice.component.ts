85200fb27fe5665fc884a72324087d1457aef73b
tike <puodsea@gmail.com>
2017-09-04 19:32:49 +0900

    Add PracticeComponent

--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/do';
+
+import { PhrasesService } from '../core/phrases.service';
+import { Phrase } from '../core/phrase';
 
 @Component({
   selector: 'gp-practice',
@@ -6,10 +13,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./practice.component.scss']
 })
 export class PracticeComponent implements OnInit {
+  section: number;
+  phrases: Phrase[];
+  scores: [number, boolean][];
+  phraseIdx: number;
+  isEnd: boolean;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private phrasesService: PhrasesService
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap
+      .do((params) => {
+        this.phraseIdx = 0;
+        this.scores = [];
+        this.section = +params.get('id');
+        this.isEnd = false;
+      })
+      .switchMap(
+        params => this.phrasesService.getSection(this.section)
+      )
+      .subscribe(
+        phrases => this.phrases = this.phrasesService.shuffle(phrases),
+        error => alert(error.message)
+      );
   }
 
+  get currentPhrase() { return this.phrases[this.phraseIdx]; }
+
+  next(decision: [number, boolean]) {
+    this.scores.push(decision);
+    if (this.phraseIdx < this.phrases.length - 1) {
+      this.phraseIdx++;
+    } else {
+      this.isEnd = true;
+    }
+  }
+
+  // only for development
+  end() {
+    let scores: [number, boolean][] = [];
+    this.phrases.forEach(p => {
+      scores.push([p.number, Math.random() > 0.5])
+    });
+    this.scores = scores;
+    this.phraseIdx = this.phrases.length - 1
+    this.next([this.phrases[this.phrases.length - 1].number, Math.random() > 0.5]);
+  }
 }
