983c0235b8e930a5d189e3d36fc2a1b3363328ac
ponsea <puodsea@gmail.com>
2017-09-05 11:25:20 +0900

    LevelsService#getLevels

--- src/app/core/levels.service.ts
+++ src/app/core/levels.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Level, LevelsResponse } from './level';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+
+import { AppInfoService } from './app-info.service';
 
 @Injectable()
 export class LevelsService {
+  private levelsCach: BehaviorSubject<Level[]> = new BehaviorSubject(null);
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private appInfo: AppInfoService
+  ) { }
 
+  getLevels(): Observable<Level[]> {
+    if (this.levelsCach.getValue() == null) {
+      let url = this.appInfo.apiUrl + "/levels"
+      return this.http.get<LevelsResponse>(url)
+        .map(response => response.data as Level[])
+        .do(levels => this.levelsCach.next(levels));
+    } else {
+      return this.levelsCach;
+    }
+  }
 }
