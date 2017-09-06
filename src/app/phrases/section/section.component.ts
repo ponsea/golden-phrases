0da7ec18914befb8aea4d77c57e15aa7b290e427
ponsea <puodsea@gmail.com>
2017-09-06 10:38:11 +0900

    Make it possible to use URL like "/phrases;section_id=?;index=?"

--- src/app/phrases/section/section.component.ts
+++ src/app/phrases/section/section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, HostListener } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { Phrase } from '../../core/phrase';
 import { PhrasesService } from '../../core/phrases.service';
@@ -11,12 +12,28 @@ import { PhrasesService } from '../../core/phrases.service';
 export class SectionComponent implements OnInit {
   @Input() sectionId: number;
   phrases: Phrase[] = [];
+  phraseIdx = 0;
 
-  constructor(private phrasesService: PhrasesService) { }
+  constructor(
+    private phrasesService: PhrasesService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
     this.phrasesService.getSection(this.sectionId)
       .subscribe(phrases => this.phrases = phrases,
                  err => alert(err.message));
+
+    this.route.paramMap
+      .subscribe(params => {
+        let i = params.get("index"); // maybe "1"-"50" or "" or null
+        this.phraseIdx = i ? +i - 1 : 0;
+      });
+  }
+
+  updateUrl(i: number) {
+    let params = { section_id: this.sectionId, index: i + 1 };
+    this.router.navigate(['/phrases', params], { replaceUrl: true });
   }
 }
