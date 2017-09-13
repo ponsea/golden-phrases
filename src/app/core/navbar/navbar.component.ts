032e94c30a55e2cb2a76f18d43b084c8bf42481f
ponsea <puodsea@gmail.com>
2017-09-13 20:21:52 +0900

    Add user name to NavbarComponent

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { AppInfoService } from '../app-info.service';
+import { AuthService } from '../auth.service';
+import { User } from '../user';
 
 @Component({
   selector: 'gp-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-  title = "";
+export class NavbarComponent implements OnInit {
+  title: string;
+  userLoginedIn: boolean;
+  currentUser: User;
 
-  constructor(appInfo: AppInfoService) {
-    this.title = appInfo.title;
+  constructor(
+    private appInfo: AppInfoService,
+    private authService: AuthService
+  ) { }
+
+  ngOnInit() {
+    this.title = this.appInfo.title;
+    this.authService.userLoginedInObservation
+      .subscribe(loginedIn => this.userLoginedIn = loginedIn);
+    this.authService.currentUserObservation
+      .subscribe(user => this.currentUser = user);
+  }
+
+  logout() {
+    this.authService.signOut()
+      .subscribe(undefined, err => {
+        alert('エラーが発生しました。しばらくの後、再度お試しください。');
+      });
   }
 }
