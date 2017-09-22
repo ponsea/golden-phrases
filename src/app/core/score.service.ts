2f002c8ac6df4e47aed626afe9b347a7590cc8df
ponsea <puodsea@gmail.com>
2017-09-22 21:26:01 +0900

    Add ScoreService#getScoreHeadings

--- src/app/core/score.service.ts
+++ src/app/core/score.service.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
 
 import { AppInfoService } from './app-info.service';
-import { Score } from './score';
+import { Score, ScoreHeading } from './score';
 
 @Injectable()
 export class ScoreService {
+  private scoreHeadings: BehaviorSubject<ScoreHeading[]> = new BehaviorSubject(null);
 
   constructor(
     private http: HttpClient,
     private appInfo: AppInfoService
   ) { }
+
+  getScoreHeadings(): Observable<ScoreHeading[]> {
+    if (this.scoreHeadings.getValue()) return this.scoreHeadings;
+
+    let url = this.appInfo.apiUrl + "/scores";
+    let params = new HttpParams().set('max', 'true');
+    return this.http.get<{data: ScoreHeading[]}>(url, {params, withCredentials: true})
+      .map(response => response.data)
+      .do(scoreHeadings => this.scoreHeadings.next(scoreHeadings));
+  }
 
   getSectionScores(sectionId: number): Observable<Score[]> {
     let url = this.appInfo.apiUrl + '/scores';
