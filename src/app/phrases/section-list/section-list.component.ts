83d6f5ad68cc3e181380d2408798947b054e111a
ponsea <puodsea@gmail.com>
2017-09-22 21:28:10 +0900

    Add score-heading progress bar to SectionListComponent

--- src/app/phrases/section-list/section-list.component.ts
+++ src/app/phrases/section-list/section-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgbPanelChangeEvent } from '@ng-bootstrap/ng-bootstrap';
+import { Observable } from 'rxjs/Observable';
 
-import { PhrasesService } from '../../core/phrases.service';
-import { Phrase } from '../../core/phrase';
+import { AuthService } from '../../core/auth.service';
+import { ScoreService } from '../../core/score.service';
+import { ScoreHeading } from '../../core/score';
 
 @Component({
   selector: 'gp-section-list',
@@ -13,21 +15,35 @@ import { Phrase } from '../../core/phrase';
 export class SectionListComponent implements OnInit {
   @Input() sections: {id: number};
   currentSectionId: number;
+  userLoginedIn: Observable<boolean>;
+  private scoreHeadings: ScoreHeading[];
 
   constructor(
-    private phrasesService: PhrasesService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private authService: AuthService,
+    private scoreService: ScoreService
   ) { }
 
   ngOnInit() {
     this.route.paramMap
       .subscribe(params => this.currentSectionId = +params.get("section_id"));
+
+    if (this.authService.userLoginedIn) {
+      this.userLoginedIn = this.authService.userLoginedInObservation;
+      this.scoreService.getScoreHeadings()
+        .subscribe(sh => this.scoreHeadings = sh);
+    }
   }
 
   updateUrl(event: NgbPanelChangeEvent) {
     if (event.nextState === true) {
       this.router.navigate(['/phrases', { section_id: event.panelId }], { replaceUrl: true });
     }
+  }
+
+  getScoreHeading(sectionId: number){
+    if (!this.scoreHeadings) return null;
+    return this.scoreHeadings.find(v => v.sectionId == sectionId);
   }
 }
