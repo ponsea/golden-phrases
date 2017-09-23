e680ac7b29daccab7646688c97ce7d7060741228
ponsea <puodsea@gmail.com>
2017-09-23 16:57:28 +0900

    Make it possible to display whether to be incorrect last time

--- src/app/phrases/section/section.component.ts
+++ src/app/phrases/section/section.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, Input, HostListener } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 import { Phrase } from '../../core/phrase';
+import { Score } from '../../core/score';
+import { ScoreService } from '../../core/score.service';
 import { PhrasesService } from '../../core/phrases.service';
 import { AuthService } from '../../core/auth.service';
 
@@ -13,16 +16,28 @@ import { AuthService } from '../../core/auth.service';
 export class SectionComponent implements OnInit {
   @Input() sectionId: number;
   phrases: Phrase[] = [];
+  lastScore: Score;
   phraseIdx = 0;
+  userLoginedIn: Observable<boolean>;
 
   constructor(
     private phrasesService: PhrasesService,
     private router: Router,
     private route: ActivatedRoute,
-    private authService: AuthService
+    private authService: AuthService,
+    private scoreService: ScoreService
   ) { }
 
+  get wasIncorrect() {
+    if (!this.lastScore) return false;
+    let currentPhrase = this.phrases[this.phraseIdx];
+    let scoreDetail = this.lastScore.scoreDetails
+                      .find(v => v.phraseId == currentPhrase.id);
+    return !scoreDetail.correct;
+  }
+
   ngOnInit() {
+    this.userLoginedIn = this.authService.userLoginedInObservation;
     this.phrasesService.getSection(this.sectionId)
       .subscribe(phrases => this.phrases = phrases,
                  err => alert(err.message));
@@ -32,6 +47,11 @@ export class SectionComponent implements OnInit {
         let i = params.get("index"); // maybe "1"-"50" or "" or null
         this.phraseIdx = i ? +i - 1 : 0;
       });
+
+    if (this.authService.userLoginedIn) {
+      this.scoreService.getRecentScore(this.sectionId)
+        .subscribe(score => this.lastScore = score);
+    }
   }
 
   updateUrl(i: number) {
