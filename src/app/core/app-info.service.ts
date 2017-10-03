b2f43a79505677b65829a2a92dfd302c7f9eaad5
ponsea <puodsea@gmail.com>
2017-10-03 10:18:07 +0900

    Add AppInfoService.apiBase and .apiPath

--- src/app/core/app-info.service.ts
+++ src/app/core/app-info.service.ts
@@ -3,5 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AppInfoService {
   readonly title = "TOEIC対策 金のフレーズ";
-  readonly apiUrl = "http://localhost:3000/v1";  // will change
+  readonly apiBase = "http://localhost:3000";  // will change
+  readonly apiPath = "/v1"
+  readonly apiUrl = this.apiBase + this.apiPath;
 }
