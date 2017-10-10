ee3cf8c1cff29d1967af7d76e6f64b0793564197
ponsea <puodsea@gmail.com>
2017-10-10 19:09:32 +0900

    Fix a access modifier in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
 
   constructor(
     private title: Title,
-    private appInfo: AppInfoService,
+    public appInfo: AppInfoService,
     private authService: AuthService
   ) { }
 
