1c645d95af90e8ef57eb88f6e4b68aaedf837be1
ponsea <puodsea@gmail.com>
2017-12-17 10:44:11 +0900

    Make it possible to listen to English only voice

--- src/app/shared/section-sound/section-sound.component.ts
+++ src/app/shared/section-sound/section-sound.component.ts
@@ -14,12 +14,24 @@ export class SectionSoundComponent implements OnInit {
   @Input() sectionId: number;
   @ViewChild('audio') audio: ElementRef;
   soundUrl = "";
+  private _enOnly = false;
 
   constructor() { }
 
+  get enOnly() { return this._enOnly; };
+  set enOnly(nextState: boolean) {
+    this._enOnly = nextState;
+    this.setSoundUrl();
+  }
+
   ngOnInit() {
+    this.setSoundUrl();
+  }
+
+  private setSoundUrl() {
+    let dir = this.enOnly ? "sections_e" : "sections";
     let num = ('0' + this.sectionId).slice(-2); // zero padding
-    this.soundUrl = `assets/sounds/sections/section${num}.mp3`;
+    this.soundUrl = `assets/sounds/${dir}/section${num}.mp3`;
   }
 
   @HostListener('document:keydown', ['$event'])
@@ -31,4 +43,5 @@ export class SectionSoundComponent implements OnInit {
       event.preventDefault();
     }
   }
+
 }
